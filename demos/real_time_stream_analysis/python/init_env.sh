a03c1da340bb90591a758f78eb20fedd9624e7d0
Sun, Xiake <xiake.sun@intel.com>
2023-09-14 19:22:52 +0800

    Update samples

--- demos/real_time_stream_analysis/python/init_env.sh
+++ demos/real_time_stream_analysis/python/init_env.sh
@@ -1,14 +1,14 @@
 echo "1. Install VLC for RTSP Server.."
 sudo apt-get install vlc
+echo " "
 
-echo "2. Init python environment ..."
+echo "2. Init python virtual environment and install python dependency ..."
 python3 -m venv devcon_demo 
 source devcon_demo/bin/activate
-
-echo "3. Install python dependency ..."
 pip3 install -r requirements.txt 
+echo " "
 
-echo "4. Download vehicle detection model from OpenVINO Open Model Zoo ..."
+echo "3. Download vehicle detection model from OpenVINO Open Model Zoo ..."
 mkdir -p models/1
 wget -P models/1 https://storage.openvinotoolkit.org/repositories/open_model_zoo/2022.1/models_bin/2/vehicle-detection-0202/FP16-INT8/vehicle-detection-0202.xml
 wget -P models/1 https://storage.openvinotoolkit.org/repositories/open_model_zoo/2022.1/models_bin/2/vehicle-detection-0202/FP16-INT8/vehicle-detection-0202.bin
